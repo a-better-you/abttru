656042d0dbb014f07fc4479da386528145124b0c
Joshua Bohde <bohdetroit@gmail.com>
2018-03-28 09:09:26 -0400

    working on rendering

--- controllers/abttruController.js
+++ controllers/abttruController.js
@@ -7,19 +7,22 @@ module.exports = function (app) {
         res.render(path.join(__dirname, "../views/main-page.handlebars"));
     });
 
-    app.get("/doctor", function (req, res) {
-        res.render(path.join(__dirname, "../views/patient.handlebars"));
-    });
+    // app.get("/doctor", function (req, res) {
+    //     res.render(path.join(__dirname, "../views/patient.handlebars"));
+    // });
 
     app.get("/profile", function (req, res) {
         res.render(path.join(__dirname, "../views/user-info.handlebars"));
     });
 
-    app.get("/api/nutriModel", function (req, res) {
+    app.get("/doctor", function (req, res) {
         db.nutriModel.findAll({
+            attributes: ['patient_name', 'fav_recipe', 'diet_option', 'diet_restriction']
         }).then(nutriModel => {
-            res.send(nutriModel.map(x => x.dataValues));
-        })
+            const hbsObj = {patients: nutriModel.map(x => x.dataValues)};
+            console.log(hbsObj);
+            res.render("patient", hbsObj);
+        });
     });
 
     app.get("/api/nutriModel/:patient_name", function (req, res) {
@@ -27,13 +30,14 @@ module.exports = function (app) {
         db.nutriModel.findAll({
             where: { patient_name: req.params.patient_name }
         }).then(nutriModel => {
-            res.send(nutriModel.map(x => x.dataValues));
+            const hbsPatient = {patients: nutriModel.map(x => x.dataValues)};
+            res.render("user-info", hbsPatient)
         })
     });
 
     app.post("/api/patient", function (req, res) {
         // Create an patient with the data available to us in req.body
-        db.nutriModel.create(req.body).then(function (recipeUpdate) {
+        db.nutriModel.create(req.body).then(function (dbPatients) {
             res.json(dbPatients);
         });
     });
--- public/js/patient-manager.js
+++ public/js/patient-manager.js
@@ -3,6 +3,7 @@ $(document).ready(function () {
     var patientList = $(".patient-container");
     var patientContainer = $(".patient-container");
     var currentURL = window.location.origin;
+    getPatients();
 
     $(document).on('click', ".add-patient", function (event) {
         alert('patient added!');
@@ -37,10 +38,6 @@ $(document).ready(function () {
 
     function createPatientRow(patientData) {
         console.log(patientData);
-        // var newTr = $("<tr>");
-        // newTr.data("patient", patientData);
-        // newTr.append("<td>" + patientData.patient_name + "</td>");
-        // return newTr;
 
         var newDiv = $("<div>");
         newDiv.data("patient", patientData);
@@ -54,12 +51,8 @@ $(document).ready(function () {
 
 
     function getPatients() {
-        $.get("/api/patient", function (data) {
-            var rowsToAdd = [];
-            for (var i = 0; i < data.length; i++) {
-                rowsToAdd.push(createPatientRow(data[i]));
-            }
-            renderPatients(rowsToAdd);
+        $.get("/doctor", function (data) {
+            
         });
     }
 
@@ -80,18 +73,3 @@ $(document).ready(function () {
         patientContainer.append(alertDiv);
     }
 });
-
-
-        //     $.ajax({
-        //         url: `https://api.edamam.com/search?q=${userQ}&app_id=76461587&app_key=b829a690de0595f2fa5b7cb02db4cd99&from=0&to=3&calories=591-722&Diet=low-carb`,
-        //         method: "GET"
-        //     }).done(function (response) {
-        //         console.log(response.hits[1]);
-        //         for (var i = 0; i < response.hits.length; i++) {
-        //             var row = $("<div>");
-        //             row.addClass("recipe");
-        //             row.append("<p>" + response.hits[i].recipe.label + "</p>");
-        //             row.append("<a href= " + response.hits[i].recipe.url + ">" + "Learn More" + "</a>");
-        //             $("#recipe-area").prepend(row);
-        //         }
-        //     });
