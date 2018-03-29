a28a15ac210ff3ef55bcf98be9e6b7de73aca979
Alberto Vargas <a.vargasmarte@gmail.com>
2018-03-28 21:27:43 -0400

    fixed carousel for profile view

--- public/js/user-info.js
+++ public/js/user-info.js
@@ -18,6 +18,7 @@ $(".search").on('click', function (event) {
         method: "GET"
     }).done(function (response) {
 
+        responseObject = response;
 
         // we create indicators - we will target this 
         // in the for loop with <li> items
@@ -29,11 +30,15 @@ $(".search").on('click', function (event) {
             "class": "img-responsive"
         });
 
+
+        // let activeCaption = $("<h3>").text(response.hits[0].recipe.label);
+
         var activeCaption = $(`<a>`);
         activeCaption.attr({
             "href": response.hits[0].recipe.url,
             "class": "btn btn-info",
             "role": "button"
+
         });
         activeCaption.text(response.hits[0].recipe.label);
 
@@ -49,6 +54,7 @@ $(".search").on('click', function (event) {
         });
         addFavBttn.text("Fave This!");
 
+
         // make function
         var addSaveBttn = $("<a>");
         addSaveBttn.addClass("btn btn default save-this");
@@ -59,44 +65,40 @@ $(".search").on('click', function (event) {
         });
         addSaveBttn.text("Save This!");
 
+
+
         // let activeImg = $("<img src = 'response.hits[0].recipe.image' alt = 'recipe'>");
         itemActive.append(activeCaption);
         itemActive.append(activeImg);
+
         itemActive.append(addSaveBttn);
+
         itemActive.append(addFavBttn);
 
+
+
         $('.carousel').carousel("pause");
+
         $("#panel-slider").show();
+
 
         console.log(response.hits[0].recipe.image);
         console.log(response.hits[1].recipe.image);
         // start of plotly code
         id = 1;
-        responseObject = response;
-        createPlots(responseObject, 0);
+
+        // createPlots(responseObject, 0);
+
+        // populate our slider with text content
 
         for (let i = 1; i < response.hits.length; i++) {
-
             console.log(response.hits[i])
-            console.log(this_id);
-            // var itemDiv = $("<div class='col-md-4 recipe'>");
-            // var img = $("<img class='img-responsive'>");
-            // img.attr("src", response.hits[i].recipe.image);
-            // itemDiv.append("<a class='btn' href=" + response.hits[i].recipe.url + ">" + 'Get Recipe' + "</a>");
-            var recipeLink = $(`<a>`);
-            recipeLink.attr("href", response.hits[i].recipe.url);
-            recipeLink.text("Get Recipe");
 
-
-            responseObject = response;
-            console.log(response);
-            console.log(responseObject);
-
-            // console.log(response.hits[i])
-            let itemDiv = $("<div class='col-md-4 recipe'>").attr({
+            let itemDiv = $("<div>").attr({
                 class: "item",
                 "data-id": i
             });
+
 
             var itemImg = $("<img>").attr({
                 "src": response.hits[i].recipe.image,
@@ -104,37 +106,48 @@ $(".search").on('click', function (event) {
                 "class": "img-responsive"
             });
 
-            // let itemCaption = $("<h3>").text(response.hits[0].recipe.label);
-
             var itemCaption = $(`<a>`);
             itemCaption.attr({
-                "href": response.hits[0].recipe.url,
+                "href": response.hits[i].recipe.url,
                 "class": "btn btn-info",
                 "role": "button"
             });
-            itemCaption.text(response.hits[0].recipe.label);
+            itemCaption.text(response.hits[i].recipe.label);
 
-            saveLink = response.hits[i].recipe.uri;
-            // make function
-            addFaveBttn = $("<a>");
-            addFaveBttn.addClass("btn btn-default fave-this");
-            addFaveBttn.attr("id", saveLink);
-            addFaveBttn.text("Fave This!");
 
+            var saveLink = response.hits[i].recipe.uri;
 
             // make function
-            addSaveBttn = $("<a>");
-            addSaveBttn.addClass("btn btn-default save-this");
-            addSaveBttn.attr("id", saveLink);
+            var addFavBttn = $("<a>");
+            // addFavBttn.addClass("btn btn default fav-this");
+            addFavBttn.attr({
+                "id": saveLink,
+                "class": "btn btn-info fav-this",
+                "role": "button"
+            });
+            addFavBttn.text("Fave This!");
+
+
+            // make function
+            var addSaveBttn = $("<a>");
+            // addSaveBttn.addClass("btn btn default save-this");
+            addSaveBttn.attr({
+                "id": saveLink,
+                "class": "btn btn-info save-this",
+                "role": "button"
+            });
             addSaveBttn.text("Save This!");
 
             itemDiv.append(itemCaption);
             itemDiv.append(itemImg);
-            itemDiv.append("<p>" + response.hits[i].recipe.label + "</p>");
-            itemDiv.append(addFaveBttn);
+
             itemDiv.append(addSaveBttn);
-            itemDiv.append(recipeLink);
-            $("#recipe-area").prepend(itemDiv);
+
+            itemDiv.append(addFavBttn);
+
+            $("#item-list").append(itemDiv);
+
+
         }
 
         // let activeDiv = $(".item active");
@@ -142,33 +155,40 @@ $(".search").on('click', function (event) {
 
         $(".right").on('click', function (event) {
             console.log('right clicked modafoca!');
+
             nextSlide++;
+
             if (nextSlide > 4) {
                 nextSlide = 0;
                 createPlots(responseObject, nextSlide);
             }
+
             // console.log(nextSlide);
             createPlots(responseObject, nextSlide);
         });
 
         $(".left").on('click', function (event) {
             console.log('right clicked modafoca!');
+
             nextSlide--;
             if (nextSlide < 0) {
                 nextSlide = 4;
                 createPlots(responseObject, nextSlide);
             }
+
             // console.log(nextSlide);
             createPlots(responseObject, nextSlide);
         });
 
-        $(".fave-this").on('click', function (event) {
+        // $("#0").addClass("active");
+
+        $(".fav-this").on('click', function (event) {
             uri = event.currentTarget.id;
             console.log(uri);
-            console.log(this_id);
-            var id = this_id;
+            console.log(thisId);
+            var id = thisId;
             $.ajax({
-                url: "api/profile/fave-recipe/" + id,
+                url: "api/patient/fav-recipe/" + id,
                 method: "PUT",
                 data: { fav_recipe: uri }
             }).done(function (response) {
@@ -180,10 +200,10 @@ $(".search").on('click', function (event) {
         $(".save-this").on('click', function (event) {
             uri = event.currentTarget.id;
             console.log(uri);
-            console.log(this_id);
-            var id = this_id;
+            console.log(thisId);
+            var id = thisId;
             $.ajax({
-                url: "api/profile/save-recipe/" + id,
+                url: "api/patient/save-recipe/" + id,
                 method: "POST",
                 data: { save_recipe: uri }
             }).done(function (response) {
@@ -192,100 +212,108 @@ $(".search").on('click', function (event) {
         });
     });
 
-    function createPlots(response, i) {
-        let arrayDigest = response.hits[i].recipe.digest;
-        let reciYield = response.hits[i].recipe.yield;
+});
 
-        let servingArray = [];
-        let firstPlot = {
-            values: [],
-            labels: []
-        };
 
-        let secondPlot = {
-            values: [],
-            labels: []
-        };
 
-        let thirdPlot = {
-            x: [],
-            y: [],
-            marker: {
-                color: ['#64609A', '#933709', '#14A989', '#469A84', '#D05340', '#436CB9', '#469496', '#3AA8C1', '#353839', '#ABAD48', '#B07080', '#BD559C', '#AA4069', '#2D5DA1', '#832A0D', '#B56917', '#E97451', '#C62D42']
-            }
-        };
+function createPlots(response, i) {
+    let arrayDigest = response.hits[i].recipe.digest;
+    let reciYield = response.hits[i].recipe.yield;
 
-        arrayDigest.forEach(nutrient => {
-            // console.log(nutrient);
-            // push nutrient servings for each nutrient
-            if (nutrient.label === "Fat" || nutrient.label === "Carbs" || nutrient.label === "Protein") {
-                firstPlot.values.push(nutrient.total / reciYield);
-                firstPlot.labels.push(nutrient.label);
+    let servingArray = [];
+    let firstPlot = {
+        values: [],
+        labels: []
+    };
+    let secondPlot = {
+        values: [],
+        labels: []
+    }
+    let thirdPlot = {
+        values: [],
+        labels: []
+    }
+    let fourthPlot = {
+        values: [],
+        labels: []
+    }
 
-                if (nutrient.label === "Fat") {
-                    nutrient.sub.forEach(fat => {
-                        secondPlot.values.push(fat.total / reciYield);
-                        secondPlot.labels.push(fat.label);
-                    })
-                }
+    arrayDigest.forEach(nutrient => {
+        // console.log(nutrient);
+        // push nutrient servings for each nutrient
+        if (nutrient.label === "Fat" || nutrient.label === "Carbs" || nutrient.label === "Protein") {
+            firstPlot.values.push(nutrient.total / reciYield);
+            firstPlot.labels.push(nutrient.label);
 
-            } else if (nutrient.label != "Cholesterol" & nutrient.label != "Folate equivalent (total)" & nutrient.label != "Folate (food)") {
-                thirdPlot.x.push(nutrient.daily / reciYield);
-                thirdPlot.y.push(nutrient.label);
-            }
-        });
-        console.log(secondPlot);
-        // console.log(plotObject);
+            if (nutrient.label === "Fat") {
+                nutrient.sub.forEach(fat => {
 
-        // process nutrition data
-        let totalNutrients = response.hits[1].recipe.digest;
-
-        // calculate totalNutrient per serving for our recipe
-        for (var key in totalNutrients) {
-            if (totalNutrients.hasOwnProperty(key)) {
-                var val = totalNutrients[key];
-                var perServing = val.quantity / 8;
+                    secondPlot.values.push(fat.total / reciYield);
+                    secondPlot.labels.push(fat.label);
+                })
 
             }
         }
 
-        // populate the website with beautiful plots
-        var data = [{
-            values: firstPlot.values,
-            labels: firstPlot.labels,
-            domain: {
-                x: [0, .48]
-            },
-            name: 'GHG Emissions',
-            hoverinfo: 'label+percent+name',
-            hole: .4,
-            type: 'pie'
-        }, {
-            values: secondPlot.values,
-            labels: secondPlot.labels,
-            text: 'CO2',
-            textposition: 'inside',
-            domain: { x: [.52, 1] },
-            name: 'CO2 Emissions',
-            hoverinfo: 'label+percent+name',
-            hole: .4,
-            type: 'pie'
-        }];
+        else if (nutrient.label != "Cholesterol" & nutrient.label != "Folate equivalent (total)" & nutrient.label != "Folate (food)") {
+            thirdPlot.x.push(nutrient.daily / reciYield);
+            thirdPlot.y.push(nutrient.label);
 
-        var layout = {
-            title: 'Macros and Fat Distribution',
-            legend: {
-                "orientation": "h"
-            },
-            height: 375,
-            width: 550,
-            grid: {
-                ygap: 0.8
-            }
-        };
+        }
 
-        Plotly.newPlot('tester', data, layout);
+    });
+    console.log(secondPlot);
+    // console.log(plotObject);
+
+    // process nutrition data
+    let totalNutrients = response.hits[1].recipe.digest;
+
+    // calculate totalNutrient per serving for our recipe
+    for (var key in totalNutrients) {
+        if (totalNutrients.hasOwnProperty(key)) {
+            var val = totalNutrients[key];
+            var perServing = val.quantity / 8;
+
+        }
     }
-});
 
 
+    // populate the website with beautiful plots
+    var data = [{
+        values: firstPlot.values,
+        labels: firstPlot.labels,
+        domain: {
+            x: [0, .48]
+        },
+        name: 'GHG Emissions',
+        hoverinfo: 'label+percent+name',
+        hole: .4,
+        type: 'pie'
+    }, {
+        values: secondPlot.values,
+        labels: secondPlot.labels,
+        text: 'CO2',
+        textposition: 'inside',
+        domain: { x: [.52, 1] },
+        name: 'CO2 Emissions',
+        hoverinfo: 'label+percent+name',
+        hole: .4,
+        type: 'pie'
+    }];
+
+    var layout = {
+        title: 'Macros and Fat Distribution',
+        legend: {
+            "orientation": "h"
+        },
+        height: 375,
+        width: 550,
+        grid: {
+            ygap: 0.8
+        }
+    };
+
+
+    Plotly.newPlot('tester', data, layout);
+
+}
